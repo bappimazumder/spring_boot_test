f7f7a64c5b3af6155eb8661326c97c5da2c5236c
Bappi Mazumder <mazumderbappi@gmail.com>
2017-09-24 22:50:31 +0600

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author bappi
+ *
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String showHome() {
+		
+		return "home";
+	}
+}
